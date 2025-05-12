40ef9b73b8f5c146fc8bf405594778bbb551e192
sandeshpd <sandeshdha26@gmail.com>
2025-05-12 12:55:40 +0530

    added search functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from inventory.models import Product
@@ -24,3 +25,11 @@ def product_list_by_category(request, category_id):
 def product_detail(request, pk):
     product = Product.objects.get(pk = pk)
     return render(request, "products/product_detail.html", {'product': product})
+
+def search_products(request):
+    query = request.GET.get('q', '')    # Get the search query from the request
+    products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query)) if query else None
+    paginator = Paginator(products, 10) # Paginate the results
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'products/product_results.html', {'products': page_obj, 'query': query})
