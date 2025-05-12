cd81336751e7f973cd01fd32054b37abd940de47
sandeshpd <sandeshdha26@gmail.com>
2025-05-12 12:55:27 +0530

    redirection to results page

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<int:pk>/', views.product_detail, name='detail'),
     path('category/<int:category_id>', views.product_list_by_category, name='products_by_category'),
+    path('search/', views.search_products, name='search'),
 ]
